401ab5b7723f4055fd3baf81a26e74ccbdf24ec5
FEDOR <tatunashvilif@mail.ru>
2021-03-13 13:40:58 +0200

    add jwt login

--- starnavi_social_network/settings.py
+++ starnavi_social_network/settings.py
@@ -26,6 +26,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SIMPLE_JWT = {
+    'UPDATE_LAST_LOGIN': True,
+}
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -106,6 +110,12 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
